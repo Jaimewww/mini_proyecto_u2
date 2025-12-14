f9adaf761ed6b3ea9470763165c513ce71b88b48
Jaimewww <jaime.landazuri@unl.edu.ec>
2025-12-14 12:02:51 -0500

    Search Result container class

---
+++ src/main/java/ed/u2/search/SearchResult.java
@@ -0,0 +1,29 @@
+package ed.u2.search;
+
+/**
+ * Clase para almacenar el resultado de una busqueda en un arreglo
+ * @author Jaime Landazuri
+ */
+
+public class SearchResult {
+    private int index;
+    private int value;
+
+    public SearchResult(int index, int value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "{Index: " + index + ", Value: " + value + "}";
+    }
+}
